61cbe52089070e14ca17b29ab8b51663d100ffe5
Alina <alina.mikhaylova.03@gmail.com>
2024-02-11 20:30:14 +0300

    я села продолжать и начала заново. перестроена логика в сторону применения стринг.

--- script.js
+++ script.js
@@ -1,95 +1,91 @@
-let numeroUno = 0;
-let numeroDos = 0;
-let operator = '+';
-let result = 0;
+let uno = '';
+let dos = '';
+let operator = '';
+let finish = false;
 
-let numberArray = [];
+const display = document.querySelector('h1');
+const numBtn = document.querySelectorAll('.num-btn');
+const operBtn = document.querySelectorAll('.oper-btn');
 
-const displayResult = document.querySelector('h1');
+// const numArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+// const operArr = ['+', '-', '*', '/'];
 
-let numbers = document.querySelectorAll('.num-btn');
-let operators = document.querySelectorAll('.oper-btn');
+numBtn.forEach((elem) => {
+    elem.addEventListener('click', () => {
+        if (dos == '' && uno.length != 11 && operator == ''){
+            uno += elem.textContent;
+            display.textContent = uno;
+        } 
 
-numbers.forEach(elem => {
-    elem.addEventListener('click', (event) => {
-        addNumberOnScreen(event.target.innerHTML);
-    });
+        if (operator != '' && dos.length != 11){
+            dos += elem.textContent;
+            display.textContent = dos;
+        }
+        
+    })
 })
-window.addEventListener('keydown', (event) => {
-    if ((isNaN(parseFloat(event.key))) ) {
-        return;
-    }
-    addNumberOnScreen(Number(event.key));
+
+operBtn.forEach((elem) => {
+    elem.addEventListener('click', () => {
+        // if (finish == true){
+        //     dos = ''
+        // }
+        operator = elem.textContent;
+        display.textContent = uno;
+        operate(uno, dos, operator);
+        dos = '';
+        // finish = false;
+    })
 })
 
-// for (let i = 0; i <= 9; i++){
-//     document.getElementById(`${i}`).onclick = () => addNumberOnScreen(`${i}`);
-//  }
+const equals = document.querySelector('.equals');
+equals.addEventListener('click', () => {
+    display.textContent = uno;
+    operate(uno, dos, operator);
+    finish = true;
+    dos = '';
+})
 
-// equal.addEventListener('click', () => {
-//     operate(numeroUno, numeroDos, operator);
-//     displayResult.textContent = numeroUno;
-// });
-
-// document.getElementById('add').onclick = () => operator = '+';
-// document.getElementById('subtract').onclick = () => operator = '-';
-// document.getElementById('multiply').onclick = () => operator = '*';
-// document.getElementById('divide').onclick = () => operator = '/';
-
-// document.getElementById('add').addEventListener('click', () => {
-//     operator = '+';
-//     clickOnOperator();
-// })
-// document.getElementById('subtract').addEventListener('click', () => {
-//     operator = '-';
-//     clickOnOperator();
-// })
-// document.getElementById('multiply').addEventListener('click', () => {
-//     operator = '*';
-//     clickOnOperator();
-// })
-// document.getElementById('divide').addEventListener('click', () => {
-//     operator = '/';
-//     clickOnOperator();
-// })
-
-// function clickOnOperator(){
-//     numeroDos = numeroUno;
-//     numberArray = [];
-//     displayResult.textContent = 0;
-// }
-
-function addNumberOnScreen(number){
-    if (numberArray.length > 10){
-        return;
-    }
-    numberArray.push(number);
-    let joined = +numberArray.join('');
-    numeroUno = joined;
-    displayResult.textContent = joined;
-}
-
-function operate(a, b, operatorPar){
-    switch (operatorPar){
+function operate(a, b, sign){
+    switch (sign){
         case '+':
-            numeroUno = a + b;
+            uno = (+a) + (+b);
             break;
         case '-':
-            numeroUno = b - a;
+            uno = (+a) - (+b);
             break;
         case '*':
-            numeroUno = a * b;
+            if (dos != ''){ 
+                uno = (+a) * (+b);
+            }
             break;
         case '/':
-            numeroUno = a / b;
+            if (dos == '0'){
+                display.textContent = 'Невозможно';
+                uno = '0';
+            }
+            if (dos != ''){ 
+                uno = (+a) / (+b);
+            }
             break;
     }
 }
-
-window.addEventListener("keydown", function(event){
-    console.log(event.key + ' ' + typeof (event.key));
-});
+// window.addEventListener("keydown", function(event){
+//     console.log(event.key + ' ' + typeof (event.key));
+// });
 // window.addEventListener("click", function(event){
 //     console.log(event.target.innerHTML);
 // });
+window.addEventListener('click', () => {
+    console.log(uno, operator, dos);
+});
 
+
+// window.addEventListener('keydown', (event) => { 
+    //     if ((isNaN(parseFloat(event.key))) ) {
+    //         return;
+    //     }
+    //     addNumberOnScreen(Number(event.key));
+    //     switchCase(event.key);
+    //     // clickOnOperator();
+    // })
